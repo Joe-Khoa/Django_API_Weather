dd70e826ed6dcbc3c2005f08a4a590a9196aeab6
unknown <demophp0205chat>
2020-03-02 01:48:32 +0700

    template

--- API_weather_app/API_weather_app/settings.py
+++ API_weather_app/API_weather_app/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC = os.path.join(BASE_DIR,'static')
- 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -25,7 +25,7 @@ SECRET_KEY = '952jrvi(do-0mcs0i9ajlt6bo1vs%gs@uap=(4me!t&#4ju2q$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dangkhoadev.pythonanywhere.com','localhost']
 
 
 # Application definition
