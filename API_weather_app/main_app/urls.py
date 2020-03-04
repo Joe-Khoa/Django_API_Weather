62f7002500c27eb23eb8639572f1c0b57aba18b9
unknown <demophp0205chat>
2020-03-04 13:36:39 +0700

    add_todo_site

--- API_weather_app/main_app/urls.py
+++ API_weather_app/main_app/urls.py
@@ -2,4 +2,7 @@ from main_app import views
 from django.urls import path
 urlpatterns = [
     path('',views.index,name='index'),
+    path('todo',views.to_do_list,name='to_do_list'),
+    path(r'todo/del/<item_id>',views.remove_item,name='del'),
+
 ]
